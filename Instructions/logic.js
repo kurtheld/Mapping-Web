c1d6ca8e8dfc530b8ca31510d2c156c658b6ee20
kurtheld <kurtheld@outlook.com>
2019-06-15 21:26:10 -0500

    commit homework

--- Instructions/logic.js
+++ Instructions/logic.js
@@ -1,61 +1,59 @@
-function createMap(bikeStations) {
+// <!-- significant help from Jonathan Randolph -->
 
-    // Create the tile layer that will be the background of our map
-    var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
-      attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
-      maxZoom: 18,
-      id: "mapbox.light",
-      accessToken: API_KEY
-    });
-  
-    // Create a baseMaps object to hold the lightmap layer
-    var baseMaps = {
-      "Light Map": lightmap
-    };
-  
-    // Create an overlayMaps object to hold the bikeStations layer
-    var overlayMaps = {
-      "Bike Stations": bikeStations
-    };
-  
-    // Create the map object with options
-    var map = L.map("map-id", {
-      center: [40.73, -74.0059],
-      zoom: 12,
-      layers: [lightmap, bikeStations]
-    });
-  
-    // Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
-    L.control.layers(baseMaps, overlayMaps, {
-      collapsed: false
-    }).addTo(map);
+const myMap = L.map("map", {
+  center: [40.924984, -115.904543],
+  zoom: 5
+});
+
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>, <br> Created by Jonathan Randolph",
+  maxZoom: 18,
+  id: "mapbox.dark",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+const GEOJSONURL = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson';
+
+function createColor(mag) {
+  return mag >= 5 ? '#ef2d2d' :
+      mag >= 4 ? '#ef632d' :
+          mag >= 3 ? '#efce2d' :
+              mag >= 2 ? '#ceef2d' :
+                  '#5def2d'
+};
+
+const legend = L.control({ position: 'bottomright' });
+
+legend.onAdd = function (map) {
+
+  var div = L.DomUtil.create('div', 'info legend'),
+      grades = [0, 1, 2, 3, 4],
+      labels = [];
+
+  // loop through our density intervals and generate a label with a colored square for each interval
+  for (var i = 0; i < grades.length; i++) {
+      div.innerHTML +=
+          '<i style="background:' + createColor(grades[i] + 1) + '"></i> ' +
+          grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
   }
-  
-  function createMarkers(response) {
-  
-    // Pull the "stations" property off of response.data
-    var stations = response.data.stations;
-  
-    // Initialize an array to hold bike markers
-    var bikeMarkers = [];
-  
-    // Loop through the stations array
-    for (var index = 0; index < stations.length; index++) {
-      var station = stations[index];
-  
-      // For each station, create a marker and bind a popup with the station's name
-      var bikeMarker = L.marker([station.lat, station.lon])
-        .bindPopup("<h3>" + station.name + "<h3><h3>Capacity: " + station.capacity + "<h3>");
-  
-      // Add the marker to the bikeMarkers array
-      bikeMarkers.push(bikeMarker);
-    }
-  
-    // Create a layer group made from the bike markers array, pass it into the createMap function
-    createMap(L.layerGroup(bikeMarkers));
-  }
-  
-  
-  // Perform an API call to the Citi Bike API to get station information. Call createMarkers when complete
-  d3.json("https://gbfs.citibikenyc.com/gbfs/en/station_information.json", createMarkers);
-  
+
+  return div;
+};
+
+legend.addTo(myMap);
+
+d3.json(GEOJSONURL).then(data => {
+
+  data.features.forEach(d => {
+      coords = d.geometry.coordinates.splice(0, 2).reverse();
+
+      L.circle(coords, {
+          fillOpacity: 0.9,
+          color: createColor(d.properties.mag),
+          fillColor: createColor(d.properties.mag),
+          radius: d.properties.mag * 15000
+      }).bindPopup(`<h2> ${d.properties.type}</h2><hr>
+      <p class = 'coords'><strong>Coordinates: </strong>${coords}</p>
+      <p><strong>Magnitude: </strong>${d.properties.mag}</p>`).addTo(myMap);
+  });
+})
